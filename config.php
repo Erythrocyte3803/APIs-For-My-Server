66c6edbe72b03edce1a356c45a93505edaa541bc
红血球AE3803 <2544390577@qq.com>
2022-04-22 11:06:40 +0800

    Add minecraft server status, performance, playerping support

--- config.php
+++ config.php
@@ -1,18 +1,24 @@
 <?php
 //API数据库设置//
-$host = 'your database host'; // mysql服务器主机地址
+$host = ''; // mysql服务器主机地址
 $port = 3306; //mysql服务器端口
-$user = 'your mysql user'; // mysql用户名
-$pass = 'your mysql pass'; // mysql用户名密码
+$user = 'root'; // mysql用户名
+$pass = ''; // mysql用户名密码
 //定义数据库//
 $luckperms = 'luckperms'; //LuckPerms数据库
 $blessing = 'skin'; //Blessing Skin Server数据库
 $ucenter = 'ucenter';//Discuz(Ucenter)数据库
+$server_stat = 'server_stat';//plan数据库
+$statuscache = 'onlinestatus';//缓存服务器在线状态
+$mcserver = ''; //服务器状态查询
+$baseport = 0;//服务器默认端口，用于检测服务器状态，如果设为bungeecord的端口，则当bungeecord宕机后，所有子服均会显示无法连接
 //支持的皮肤站，可指定多个//
 $support = array(
-    "https://skin.9cymc.cn",//九境尘域皮肤站，有皮肤和披风API
-    "https://mcskin.littleservice.cn",//LittleSkin皮肤站，有皮肤和披风API
-    "https://minotar.net",//Minotar正版皮肤站，有皮肤API
+    "https://skin.9jcy.cn",//九境尘域皮肤站，有皮肤和披风API
+    "https://mcskin.cn",//红石皮肤站，有皮肤和披风API
+    "https://skin.prin.studio",//Blessing皮肤站，有皮肤和披风API
+    "https://littleskin.cn",//LittleSkin皮肤站，有皮肤和披风API
+    "https://crafthead.net",//Minotar正版皮肤站，有皮肤API
     "https://crafatar.com",//Craftatar正版皮肤站，有皮肤和披风API
     "https://uc.zhjlfx.cn",//仅用来加载Dz论坛的头像
     "https://api.zhjlfx.cn",//方便内部调用
--- index.php
+++ index.php
@@ -7,7 +7,134 @@ $size = $_GET['size'];
 $email = $_GET['email'];
 $uuid_o = $_GET['uuid'];
 $uuid = str_replace('-', '', $uuid_o);
+$skinhash = $_GET['skinhash'];
+$port = (int)$_GET['port'];
+global $mcserver, $baseport;
 switch ($type) {
+    case 'severstat';
+        header('content-type:application/json;charset=utf8');
+        switch ($method) {
+            case 'perf';
+                if ($port == 0) {
+                    $port = $baseport;
+                }
+                $pingdata = ($mcping->GetStatus($mcserver, $port)->Response());
+                $isonline = $pingdata['online'];
+                if (!$isonline) {
+                    $onlinestat = 0;
+                } else {
+                    $onlinestat = 1;
+                }
+                $db->cacheOnlineStatus($onlinestat, $port);
+                $online = $db->readOnlineStatus($port);
+                $onlinep = $db->readOnlineStatus($baseport);
+                if ($online == 1 && $onlinep == 1) {
+                    $perfdata = $db->getServerStats($port);
+                    $getdata = array(
+                        'servername' => $perfdata[0],
+                        'tps' => $perfdata[1],
+                        'cpu' => $perfdata[2],
+                        'ram' => $perfdata[3],
+                        'entities' => $perfdata[4],
+                        'chunks' => $perfdata[5]
+                    );
+
+                    echo json_encode($getdata, JSON_PRETTY_PRINT);
+                    exit;
+                } else {
+                    $getdata = array(
+                        'servername' => 'none',
+                        'tps' => 0,
+                        'cpu' => 0,
+                        'ram' => 0,
+                        'entities' => 0,
+                        'chunks' => 0
+                    );
+                    echo json_encode($getdata, JSON_PRETTY_PRINT);
+                    exit;
+                }
+                break;
+            case 'playerping';
+                if ($port == 0) {
+                    $port = $baseport;
+                }
+                $pingdata = ($mcping->GetStatus($mcserver, $port)->Response());
+                $isonline = $pingdata['online'];
+                if (!$isonline) {
+                    $onlinestat = 0;
+                } else {
+                    $onlinestat = 1;
+                }
+                $db->cacheOnlineStatus($onlinestat, $port);
+                $online = $db->readOnlineStatus($port);
+                $onlinep = $db->readOnlineStatus($baseport);
+                if ($isonline == 1 && $onlinep == 1) {
+                    $avgping = $db->getPlayerPing($port);
+                    if ($avgping != false) {
+                        $avgpingdata = array(
+                            'max' => $avgping[0],
+                            'min' => $avgping[1],
+                            'avg' => $avgping[2]
+                        );
+                        echo json_encode($avgpingdata, JSON_PRETTY_PRINT);
+                        exit;
+                    } else {
+                        $avgpingdata = array(
+                            'max' => 0,
+                            'min' => 0,
+                            'avg' => 0
+                        );
+                        echo json_encode($avgpingdata, JSON_PRETTY_PRINT);
+                        exit;
+                    }
+                } else {
+                    $avgpingdata = array(
+                        'max' => 0,
+                        'min' => 0,
+                        'avg' => 0
+                    );
+                    echo json_encode($avgpingdata, JSON_PRETTY_PRINT);
+                    exit;
+                }
+                break;
+        }
+        break;
+    case 'mcping';
+        header('content-type:application/json;charset=utf8');
+        if ($port == 0) {
+            $port = $baseport;
+        }
+        $pingdata = ($mcping->GetStatus($mcserver, $port)->Response());
+        $isonline = $pingdata['online'];
+        if (!$isonline) {
+            $onlinestat = 0;
+        } else {
+            $onlinestat = 1;
+        }
+        $db->cacheOnlineStatus($onlinestat, $port);
+        $online = $db->readOnlineStatus($port);
+        $onlinep = $db->readOnlineStatus($baseport);
+        $onlineplayers = $pingdata['players'];
+        $maxplayers = $pingdata['max_players'];
+        $serverlat = $pingdata['ping'];
+        if ($online == 1 && $onlinep == 1) {
+            $serverstatus = array(
+                'onlineplayers' => $onlineplayers,
+                'maxplayers' => $maxplayers,
+                'timeout' => $serverlat
+            );
+            echo json_encode($serverstatus, JSON_PRETTY_PRINT);
+            exit;
+        } else {
+            $serverstatus = array(
+                'onlineplayers' => 0,
+                'maxplayers' => 0,
+                'timeout' => 0
+            );
+            echo json_encode($serverstatus, JSON_PRETTY_PRINT);
+            exit;
+        }
+        break;
     case 'getskin';
         header("Content-Type: image/png; charset=utf-8");
         switch ($method) {
@@ -27,21 +154,21 @@ switch ($type) {
         }
         break;
     case 'getjson';
-        //header('content-type:application/json;charset=utf8');
+        header('content-type:application/json;charset=utf8');
         if ($db->isUuidExist($uuid)) {
             if ($db->isVerified($uuid) == false) {
                 $skininfo = skinhash::skininfo($uuid);
                 $ucplayer = $db->getUserFromUcenter($uuid);
-                if (!$db->isSkinHashSameAsDatabase($skininfo[0],$uuid)) {
-                    $db->creSkinData($skininfo[0],$skininfo[1],$skininfo[2],$ucplayer,$uuid);
-                    $db->updatePlayer($ucplayer,$uuid);
+                if (!$db->isSkinHashSameAsDatabase($skininfo[0], $uuid)) {
+                    $db->creSkinData($skininfo[0], $skininfo[1], $skininfo[2], $ucplayer, $uuid);
+                    $db->updatePlayer($ucplayer, $uuid);
                     $skindata = $db->getSkinInfo($uuid);
                 } else {
-                    $db->updateCape($skininfo[1],$uuid);
-                    $db->updatePlayer($ucplayer,$uuid);
+                    $db->updateCape($skininfo[1], $uuid);
+                    $db->updatePlayer($ucplayer, $uuid);
                     $skindata = $db->getSkinInfo($uuid);
                 }
-                $skinjson = getSkinJson($skindata[5],$skindata[4],$skindata[3],$skindata[0],$skindata[2],$skindata[1],$skinurl);
+                $skinjson = getSkinJson($skindata[5], $skindata[4], $skindata[3], $skindata[0], $skindata[2], $skindata[1], $skinurl);
                 echo $skinjson;
                 break;
             } else {
@@ -50,124 +177,143 @@ switch ($type) {
             }
             break;
         } else {
-            echo '{"timestamp":1597140006232,"profileId":"cabb3c5768f33e1c87a407ad30e355bf","profileName":"noavatar","isPublic":true,"textures":{"SKIN":{"url":"https://api.zhjlfx.cn/textures/83cee5ca6afcdb171285aa00e8049c297b2dbeba0efb8ff970a5677a1b644032","metadata":{"model":"slim"}}}}';
+            $skinjson = mojangskin::getskin($uuid);
+            echo $skinjson;
             break;
         }
-    case 'getcape';
-        header("Content-Type: image/png; charset=utf-8");
-        switch ($method) {
-            case 'name';
-                echo getCapeByName($user);
-                break;
-            case 'uuid';
-                if (!$db->isUuidExist($uuid)) {
-                    echo getCapeByUuid($uuid);
-                    exit;
-                } else {
-                    $player = $db->getNameByUuid($uuid);
-                    echo getCapeByName($player);
-                    exit;
-                }
-                break;
-        }
-        break;
-    case 'getavatar';
-        header("Content-Type: image/png; charset=utf-8");
-        switch ($method) {
-            case 'name';
-                if (!$db->isPlayerGenuine($user)) {
-                    echo getAvatarByName($user,$size);
+        case 'getcape';
+            header("Content-Type: image/png; charset=utf-8");
+            switch ($method) {
+                case 'name';
+                    echo getCapeByName($user);
                     break;
-                }else{
-                    $puuid = file_get_contents("https://api.zhjlfx.cn/?type=getuuid&method=name&name=".$user);
-                    echo file_get_contents("https://api.zhjlfx.cn/?type=getavatar&method=uuid&uuid=".$puuid."&size=".$size);
-                    break;
-                }
-
-            case 'uuid';
-                if (!$db->isUuidExist($uuid)) {
-                    echo getAvatarByUuid($uuid,$size);
-                    exit;
-                } else {
-                    if (!$db->isVerified($uuid)) {
-                        $player = $db->getNameByUuid($uuid);
-                        echo getAvatarByName($player,$size);
+                case 'uuid';
+                    if (!$db->isUuidExist($uuid)) {
+                        echo getCapeByUuid($uuid);
                         exit;
                     } else {
-                        echo getAvatarByUuid($uuid,$size);
+                        $player = $db->getNameByUuid($uuid);
+                        echo getCapeByName($player);
                         exit;
                     }
-                }
-                break;
-            case 'dz';
-                echo getAvatarFromDz($user);
-                exit;
-        }
-        break;
-    case 'getuuid';
-        switch ($method) {
-            case 'name';
-                if (!$db->getUuidByName($user)) {
-                    echo getMojangUuid::uuid($user);
-                    exit;
-                } else {
-                    if ($db->isPlayerGenuine($user)) {
-                        if (getMojangUuid::uuid($user) == '') {
-                            echo $db->getUuidByName($user);
+                    break;
+            }
+            break;
+        case 'getavatar';
+            header("Content-Type: image/png; charset=utf-8");
+            switch ($method) {
+                case 'name';
+                    if (!$db->isPlayerGenuine($user)) {
+                        echo getAvatarByName($user, $size);
+                        break;
+                    } else {
+                        $puuid = file_get_contents("https://api.zhjlfx.cn/?type=getuuid&method=name&name=".$user);
+                        echo file_get_contents("https://api.zhjlfx.cn/?type=getavatar&method=uuid&uuid=".$puuid."&size=".$size);
+                        break;
+                    }
+
+                    case 'uuid';
+                        if (!$db->isUuidExist($uuid)) {
+                            echo getAvatarByUuid($uuid, $size);
                             exit;
                         } else {
-                            echo getMojangUuid::uuid($user);
-                            exit;
+                            if (!$db->isVerified($uuid)) {
+                                $player = $db->getNameByUuid($uuid);
+                                echo getAvatarByName($player, $size);
+                                exit;
+                            } else {
+                                echo getAvatarByUuid($uuid, $size);
+                                exit;
+                            }
                         }
-
-                    } else {
-                        echo $db->getUuidByName($user);
+                        break;
+                    case 'dz';
+                        echo getAvatarFromDz($user);
                         exit;
+                }
+                break;
+                case 'getuuid';
+                    switch ($method) {
+                        case 'name';
+                            if (!$db->getUuidByName($user)) {
+                                echo getMojangUuid::uuid($user);
+                                exit;
+                            } else {
+                                if ($db->isPlayerGenuine($user)) {
+                                    if (getMojangUuid::uuid($user) == '') {
+                                        echo $db->getUuidByName($user);
+                                        exit;
+                                    } else {
+                                        echo getMojangUuid::uuid($user);
+                                        exit;
+                                    }
+
+                                } else {
+                                    echo $db->getUuidByName($user);
+                                    exit;
+                                }
+                                break;
+                            }
+                            break;
+                        case 'email';
+                            if ($db->getSkinUuid($email)) {
+                                echo $db->getSkinUuid($email);
+                                exit;
+                            } else {
+                                echo '';
+                                exit;
+                            }
+                            break;
                     }
-                    break;
+                    case 'gen';
+                        switch ($method) {
+                            case 'name';
+                                if (!$db->isPlayerGenuine($user)) {
+                                    echo '玩家 '.$user.' 不是Minecraft正版玩家';
+                                    exit;
+                                } else {
+                                    echo '玩家 '.$user.' 是Minecraft正版玩家';
+                                    exit;
+                                }
+                                break;
+                            case 'uuid';
+                                $result = $db->isUuidGenuine($uuid);
+                                echo '玩家名：'.$result[0].'<br>玩家UUID：'.$result[1].'<br>是否为正版玩家：'.$result[2];
+                                break;
+                        }
+                        break;
+                    case 'namecode';
+                        switch ($method) {
+                            case 'encode';
+                                if (!$db->isUserExist($user)) {
+                                    break;
+                                } else {
+                                    $encoded = encode($user);
+                                    echo $encoded;
+                                    break;
+                                }
+                                case 'decode';
+                                    $decoded = decode($user);
+                                    echo $decoded;
+                                    break;
+                        }
+                        break;
+                    case 'getcsl';
+                        header('content-type:application/json;charset=utf8');
+                        $csldata = $db->getCSLData($user);
+                        echo getCSL($csldata);
+                        break;
+                    case 'skinimage';
+                        header('Content-type: image/png');
+                        $path = $_SERVER['DOCUMENT_ROOT'].'/textures/'.$skinhash;
+                        $image = file_get_contents($path);
+                        $size = filesize($path);
+                        header('Content-length: '.$size);
+                        echo $image;
+                        break;
+                    case 'skin2head';
+                        $skinurl = "https://skin.prin.studio/skin/".$user.".png";
+                        $data = skin2head::getskin($skinurl, $size);
+                        echo $data;
+
                 }
-                break;
-            case 'email';
-                if ($db->getSkinUuid($email)) {
-                    echo $db->getSkinUuid($email);
-                    exit;
-                } else {
-                    echo '';
-                    exit;
-                }
-                break;
-        }
-    case 'gen';
-        switch ($method) {
-            case 'name';
-                if (!$db->isPlayerGenuine($user)) {
-                    echo '玩家 '.$user.' 不是Minecraft正版玩家';
-                    exit;
-                } else {
-                    echo '玩家 '.$user.' 是Minecraft正版玩家';
-                    exit;
-                }
-                break;
-            case 'uuid';
-                $result = $db->isUuidGenuine($uuid);
-                echo '玩家名：'.$result[0].'<br>玩家UUID：'.$result[1].'<br>是否为正版玩家：'.$result[2];
-                break;
-        }
-        break;
-    case 'namecode';
-        switch ($method) {
-            case 'encode';
-                if (!$db->isUserExist($user)) {
-                    break;
-                } else {
-                    $encoded = encode($user);
-                    echo $encoded;
-                    break;
-                }
-            case 'decode';
-                $decoded = decode($user);
-                echo $decoded;
-                break;
-        }
-        break;
-}
